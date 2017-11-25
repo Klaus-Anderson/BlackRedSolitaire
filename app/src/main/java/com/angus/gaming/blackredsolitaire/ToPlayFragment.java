735a6ff6579949c70951dfeec920de6a66ec9455
Harry Cliff <harryacliff@gmail.com>
2017-11-24 22:30:05 -0500

    Created new ToPlayFragment which will be used for displaying how to play the game.
    Signed-off-by: Harry Cliff <harryacliff@gmail.com>

---
+++ app/src/main/java/com/angus/gaming/blackredsolitaire/ToPlayFragment.java
@@ -0,0 +1,29 @@
+package com.angus.gaming.blackredsolitaire;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ToPlayFragment extends Fragment {
+
+
+    public ToPlayFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_to_play, container, false);
+    }
+
+}
